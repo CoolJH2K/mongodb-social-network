3e834f6a62add26aed60a1b81a0ccaaf426cf04b
Janna Harmon <harmonjanna00@gmail.com>
2023-05-19 10:03:46 -0400

    Added most of the required files.

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,10 @@
 const mongoose = require("mongoose");
 
 // Connect to MongoDB via mongoose
-mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/socialmedia");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/socialmedia", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 // Export connection
 module.exports = mongoose.connection;
